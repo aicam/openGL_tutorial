110fc060a74b810156ad984950ae277a952c93ca
ali <a_icam@yahoo.com>
2020-04-14 23:28:22 +0430

    finally mouse position found :))))))))))

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 unsigned int drawValue = GL_LINE_LOOP;
 float positions[8] = {
-        427, 284430,
+        650, 701,
         200, 300,
         150, 200,
         200, 250
@@ -14,40 +14,46 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
         drawValue = drawValue == GL_LINE_LOOP ? GL_POLYGON : GL_LINE_LOOP;
-        positions[0] = 0.0f;
     }
 }
+int SCREEN_WIDTH = 640;
+int SCREEN_HEIGHT = 480;
+int WINDOW_WIDTH = 0;
+int WINDOW_HEIGHT = 0;
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
-//    if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-
+    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        double xpos, ypos;
+        //getting cursor position
+        glfwGetCursorPos(window, &xpos, &ypos);
+        cout << "Cursor Position at (" << xpos << " : " <<  WINDOW_HEIGHT - ypos << endl;
+    }
 }
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
     cout << xpos << " " << ypos;
 }
-int SCREEN_WIDTH = 640;
-int SCREEN_HEIGHT = 480;
 int main() {
     GLFWwindow* window;
     if (!glfwInit())
         cout << "Hello, World!" << std::endl;
+    auto* mode = const_cast<GLFWvidmode *>(glfwGetVideoMode(glfwGetPrimaryMonitor()));
+    SCREEN_WIDTH = mode->width;
+    SCREEN_HEIGHT = mode->height;
     window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello world!!", NULL, NULL);
     glfwMakeContextCurrent(window);
+    glfwGetWindowSize(window, &WINDOW_WIDTH, &WINDOW_HEIGHT);
     glfwSetKeyCallback(window, key_callback);
-    glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
     glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
     glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+//    glMatrixMode(GL_PROJECTION);
     glewInit();
     if (!window) {
         cout << "window wasnt created";
     }
     GLuint buffer;
-//    glGenBuffers(1, &buffer);
-//    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_DYNAMIC_DRAW);
-//    glEnableVertexAttribArray(0);
-//    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2* sizeof(float), 0);
     while (!glfwWindowShouldClose(window)) {
 //        glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_DYNAMIC_DRAW);
         glClear(GL_COLOR_BUFFER_BIT);
