2f7fdf9f8ea8a22dad9e409d5e15a90fac93fa61
ali <a_icam@yahoo.com>
2020-04-15 09:38:16 +0430

    finished

--- main.cpp
+++ main.cpp
@@ -4,35 +4,55 @@
 #include <iostream>
 using namespace std;
 unsigned int drawValue = GL_LINE_LOOP;
-float positions[8] = {
-        650, 701,
-        200, 300,
-        150, 200,
-        200, 250
+int SCREEN_WIDTH = 640;
+int SCREEN_HEIGHT = 480;
+int WINDOW_WIDTH = 0;
+int WINDOW_HEIGHT = 0;
+bool vertexMoving = false;
+int vertexIndex = 0;
+int threshold = 10;
+double positions[8] = {
+        300, 200,
+        300, 300,
+        400, 300,
+        400, 200
 };
+bool distanceMeasure(double x1, double y1, double x2, double y2) {
+    return (abs(x1 - x2) < threshold) && (abs(y1 - y2) < threshold);
+}
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
         drawValue = drawValue == GL_LINE_LOOP ? GL_POLYGON : GL_LINE_LOOP;
     }
 }
-int SCREEN_WIDTH = 640;
-int SCREEN_HEIGHT = 480;
-int WINDOW_WIDTH = 0;
-int WINDOW_HEIGHT = 0;
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
-    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    if(button == GLFW_MOUSE_BUTTON_LEFT)
     {
-        double xpos, ypos;
-        //getting cursor position
-        glfwGetCursorPos(window, &xpos, &ypos);
-        cout << "Cursor Position at (" << xpos << " : " <<  WINDOW_HEIGHT - ypos << endl;
+        if (action == GLFW_PRESS) {
+            double xpos, ypos;
+            //getting cursor position
+            glfwGetCursorPos(window, &xpos, &ypos);
+            ypos = WINDOW_HEIGHT - ypos;
+            for (int i = 0; i < 4; i++) {
+                if (distanceMeasure(positions[2 * i], positions[2 * i + 1], xpos, ypos)) {
+                    vertexMoving = true;
+                    vertexIndex = i;
+                }
+            }
+        } else {
+            vertexMoving = false;
+        }
     }
 }
 static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
 {
-    cout << xpos << " " << ypos;
+    if (vertexMoving) {
+        ypos = WINDOW_HEIGHT - ypos;
+        positions[2* vertexIndex] = xpos;
+        positions[2* vertexIndex + 1] = ypos;
+    }
 }
 int main() {
     GLFWwindow* window;
@@ -46,6 +66,7 @@ int main() {
     glfwGetWindowSize(window, &WINDOW_WIDTH, &WINDOW_HEIGHT);
     glfwSetKeyCallback(window, key_callback);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
     glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);
     glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
 //    glMatrixMode(GL_PROJECTION);
@@ -58,7 +79,7 @@ int main() {
 //        glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_DYNAMIC_DRAW);
         glClear(GL_COLOR_BUFFER_BIT);
         glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(2, GL_FLOAT, 0, positions);
+        glVertexPointer(2, GL_DOUBLE, 0, positions);
         glDrawArrays(drawValue, 0, 4);
         glfwSwapBuffers(window);
         glfwPollEvents();
